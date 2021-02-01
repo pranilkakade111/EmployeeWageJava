61f73661153d9fea2a5c4dcb592d2c73bc058938
Pranil Kakade <pranilkakade111@gmail.com>
2021-02-02 01:12:44 +0530

    Showing Employee is present or not

--- EmployeeWageJava/src/EmployeeWageUs.java
+++ EmployeeWageJava/src/EmployeeWageUs.java
@@ -1,11 +1,24 @@
 
 public class EmployeeWageUs 
 {
-
+  public void EmployeeWageUC1()
+  {
+	  int IsPresent=1;
+	  double empCheck = Math.floor(Math.random() * 10) % 2;
+	  if(IsPresent==empCheck)
+	  {
+		  System.out.println("Employee is Present");
+	  }
+	  else
+	  {
+		  System.out.println("Employee is Abscent");
+	  }
+  }
 	public static void main(String[] args) 
 	{
-      System.out.println("Welcometo Employee Wage Computation Program");		
-
+      System.out.println("Welcometo Employee Wage Computation Program");
+      EmployeeWageUs obj=new EmployeeWageUs();
+      obj.EmployeeWageUC1();
 	}
 
 }
